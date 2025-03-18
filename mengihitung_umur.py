49ba36216684b5834ff74baf6fe1450acc14528d
ririnekas <ririneka2020@gmail.com>
2025-03-18 08:47:37 +0700

    first commit

---
+++ mengihitung_umur.py
@@ -0,0 +1,5 @@
+print("menghitung umur")
+tahun_sekarang=int (input("masukkan  tahun sekarang:"))
+tahun_lahir=int (input("masukkan tahun lahir"))
+umur= tahun_sekarang-tahun_lahir
+print ("umur saya sekarang" ,umur, "tahun")
